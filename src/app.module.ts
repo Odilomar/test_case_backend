1c95aab0c6961fdbaca977078625abdd08494077
Odilomar <odilomarjunior.contact@gmail.com>
2023-02-28 21:34:04 -0300

    feat: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('postgres.host'),
+          port: configService.get('postgres.port'),
+          username: configService.get('postgres.user'),
+          password: configService.get('postgres.pass'),
+          database: configService.get('postgres.db'),
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,52 @@
+import { config } from 'dotenv';
+import { env } from 'process';
+
+/**
+ * Configurações e variáveis de ambiente
+ */
+export default (): Configuration => {
+  config();
+
+  return {
+    postgres: {
+      host: env.DB_HOST,
+      port: parseInt(env.DB_PORT),
+      user: env.DB_USER,
+      pass: env.DB_PASS,
+      db: env.DB_NAME,
+    },
+    environment: {
+      nodeEnv: env.NODE_ENV,
+      port: parseInt(env.PORT),
+    },
+    typeorm: {
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+      logger: process.env.TYPEORM_LOGGER,
+      entities: process.env.TYPEORM_ENTITIES,
+    },
+  };
+};
+
+/**
+ * Tipos da configuração
+ */
+type Configuration = {
+  postgres: {
+    host: string;
+    port: number;
+    user: string;
+    pass: string;
+    db: string;
+  };
+  environment: {
+    nodeEnv: string;
+    port: number;
+  };
+  typeorm: {
+    synchronize: boolean;
+    logging: boolean;
+    logger: string;
+    entities: string;
+  };
+};
